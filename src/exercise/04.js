262c14efb3b54f9987a5c870ff7396f8292eb89b
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2021-07-13 16:46:43 -0700

    04.extra-01

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -4,33 +4,60 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
+// This is going to take any number of functions, and then it's going to return a function that takes any number 
+// of arguments. I don't care what those arguments are. We'll take those functions. For each of those, we'll take that function.
+const callAll =
+  (...fns) =>
+  (...args) =>
+    fns.forEach(fn => fn?.(...args))
+
 function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  // 🐨 Add a property called `togglerProps`. It should be an object that has
-  // `aria-pressed` and `onClick` properties. (X)
-  // 💰 {'aria-pressed': on, onClick: toggle}
+  // "Give me a function that will call all of the functions I pass you onClick and toggle." It'll call onClick, 
+  // and then it'll call toggle. It'll only call onClick if that function actually exists.
+  function getTogglerProps({onClick, ...props} = {}) {
+    return {
+      'aria-pressed': on,
+      onClick: callAll(onClick, toggle),
+      ...props,
+    }
+  }
 
-  // we get the toggler props, we spread that across the position of the props of our switch, 
-  // and we do the same for the props. Now, we have an accessible and working switch and toggle button.
   return {
     on,
     toggle,
-    togglerProps: {
-      'aria-pressed': on,
-      onClick: toggle,
-    },
+    getTogglerProps,
   }
 }
 
+// function App() {
+//   const {on, togglerProps} = useToggle()
+//   return (
+//     <div>
+//       <Switch on={on} {...togglerProps} />
+//       <hr />
+//       <button aria-label="custom-button" {...togglerProps}>
+//         {on ? 'on' : 'off'}
+//       </button>
+//     </div>
+//   )
+// }
+
 function App() {
-  const {on, togglerProps} = useToggle()
+  const {on, getTogglerProps} = useToggle()
   return (
     <div>
-      <Switch on={on} {...togglerProps} />
+      <Switch {...getTogglerProps({on})} />
       <hr />
-      <button aria-label="custom-button" {...togglerProps}>
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.info('onButtonClick'),
+          id: 'custom-button-id',
+        })}
+      >
         {on ? 'on' : 'off'}
       </button>
     </div>
