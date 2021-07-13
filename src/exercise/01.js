0875da9c101fcee674559da6deea855a0fec4240
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2021-07-12 19:09:34 -0700

    01 file

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -73,14 +73,26 @@ function useUser() {
   return context
 }
 
-// 🐨 add a function here called `updateUser`
-// Then go down to the `handleSubmit` from `UserSettings` and put that logic in
-// this function. It should accept: dispatch, user, and updates
+// We'll await that and get our updated user. Then we can do our dispatch on that updated user, 
+// and then return the updated user, so we can resolve the promise with that updated user. 
+// Then in the event of an error, we can handle that by dispatching that error. 
+// We'll return a promise.reject of that error, or you could also throw that error.
+async function updatedUser(dispatch, user, updates) {
+  dispatch({type: 'start update', updates})
+  try {
+    const updatedUser = await userClient.updateUser(user, updates)
+    dispatch({type: 'finish update', updatedUser})
+    return updatedUser
+  } catch (error) {
+    dispatch({type: 'fail update', error})
+    throw error
+  }
+}
 
-// export {UserProvider, useUser}
+// export {UserProvider, useUser, updateUser}
 
 // src/screens/user-profile.js
-// import {UserProvider, useUser} from './context/user-context'
+// import {UserProvider, useUser, updateUser} from './context/user-context'
 function UserSettings() {
   const [{user, status, error}, userDispatch] = useUser()
 
@@ -97,12 +109,9 @@ function UserSettings() {
 
   function handleSubmit(event) {
     event.preventDefault()
-    // 🐨 move the following logic to the `updateUser` function you create above
-    userDispatch({type: 'start update', updates: formState})
-    userClient.updateUser(user, formState).then(
-      updatedUser => userDispatch({type: 'finish update', updatedUser}),
-      error => userDispatch({type: 'fail update', error}),
-    )
+    updatedUser(userDispatch, user, formState).catch(error => {
+      // ignore the errror
+    })
   }
 
   return (
