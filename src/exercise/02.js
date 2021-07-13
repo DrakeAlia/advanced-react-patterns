57060a4a28578aaae8fc3252872bda99c9e412c7
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2021-07-12 22:17:58 -0700

    added notes 02.extra

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -7,6 +7,7 @@ import {Switch} from '../switch'
 // 📜 https://reactjs.org/docs/react-api.html#reactchildren
 // 📜 https://reactjs.org/docs/react-api.html#cloneelement
 
+// we just trusted in faith that we would get the props that we needed for each one of these components. 
 // Some of those props are provided to us by the user of our component. 
 // Others of these props are provided implicitly. 
 // Thanks to the toggle component we're going to be rendered inside of.
@@ -14,10 +15,7 @@ function Toggle({children}) {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  // This toggle component hands those props to us by taking all of its children and mapping those to new children, 
-  // which are cloned copies of that original child with these additional props passed implicitly. 
-  // At least, it's implicit from the perspective of the users, 
-  // and it's explicit from the perspective of this toggle component.
+  
   return React.Children.map(children, child => {
     if (typeof child.type === 'string') {
       return child
@@ -31,13 +29,9 @@ function Toggle({children}) {
   })
 }
 
-// Accepts `on` and `children` props and returns `children` if `on` is true (X)
+
 const ToggleOn = ({on, children}) => (on ? children : null)
-
-// Accepts `on` and `children` props and returns `children` if `on` is false (X)
 const ToggleOff = ({on, children}) => (on ? null : children)
-
-// Accepts `on` and `toggle` props and returns the <Switch /> with those props. (X)
 const ToggleButton = ({on, toggle}) => <Switch on={on} onClick={toggle} />
 
 function App() {
